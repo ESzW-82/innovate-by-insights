791b142824b97fde9219545a9d0b057acd2cb716
ESzW-82 <babelstein82@gmail.com>
2025-07-20 10:31:33 +0200

    Create App.jsx

---
+++ src/src/src/src/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to Innovate By Insights</h1>
+      <p>This site is currently under construction. Stay tuned!</p>
+    </div>
+  );
+}
