6abc6190171d35cdf92860246fdcd5158fd96274
ESzW-82 <babelstein82@gmail.com>
2025-07-20 10:55:40 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <h1>Welcome to Innovate By Insights</h1>
+      <p>This is your landing page powered by Vite + React!</p>
+    </div>
+  );
+}
+
+export default App;
